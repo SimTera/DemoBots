62a4dd5b117eb7d256806f4e341501fc2566034a
Víctor Múnera <vmunerav@icloud.com>
2026-04-24 17:46:46 +0200

    Initial Commit

---
+++ DemoBots macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  DemoBots macOS
+//
+//  Created by Victor Munera on 24/4/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
